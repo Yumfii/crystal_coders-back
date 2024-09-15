e8fac9f099848afcfc5b78634d09dd367edb0533
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-09-15 21:20:58 +0200

    bug fix

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -3,7 +3,6 @@
 import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
