54285ca54d1f384c700d9d9acfe9b2144e1491b4
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-09-15 21:16:31 +0200

    auth fix

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -3,7 +3,7 @@
 import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+  name: Joi.string().min(3).max(30),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
