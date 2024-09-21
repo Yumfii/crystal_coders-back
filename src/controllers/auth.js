a8741864f242163914715626198a6347820c080e
Olek Miedviediev <medvedevbasket@gmail.com>
2024-09-21 22:50:09 +0200

    fix bug

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -21,7 +21,7 @@ export const registerUserController = async (req, res) => {
 
   res.status(201).json({
     status: 201,
-    message: 'Successfully registered a user!',
+    message: 'Successfully registered a user! Please verefy your email!',
     data: {
       user,
       accessToken: session.accessToken,
