8dc666705e07e4681df2f161273940b6578890da
Maksym-Yaroshenko <yiaroshenko.max@gmail.com>
2024-09-23 01:03:04 +0300

    cors

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -165,7 +165,7 @@ export const handleAuthCallback = async (req, res) => {
 
     setupSession(res, session);
 
-    res.redirect('http://localhost:5173/tracker');
+    res.redirect('https://crystal-coders-front.vercel.app/tracker');
   } catch (error) {
     console.error('Error during Google OAuth:', error);
     res.status(500).send('Error during Google OAuth');
